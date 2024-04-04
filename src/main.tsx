5b0a1f945c1d5930dc079cfa28f3e0c553b57bac
HomeMac <mauserez@mail.ru>
2024-04-04 16:39:30 +0300

    try to add error

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,22 @@ import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./shared/redux/store";
 import "./index.css";
-
+err;
 const container = document.getElementById("root");
 
 if (container) {
-  const root = createRoot(container);
+	const root = createRoot(container);
 
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-  );
+	root.render(
+		<React.StrictMode>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</React.StrictMode>
+	);
 } else {
-  throw new Error(
-    `Root element with ID 'root' was not found in the document.
-		Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.`,
-  );
+	throw new Error(
+		`Root element with ID 'root' was not found in the document.
+		Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.`
+	);
 }
