931cdfaa7ed9a962902d2ece0907215b2b26019a
HomeMac <mauserez@mail.ru>
2024-04-04 15:53:47 +0300

    err

--- src/shared/api/words/index.ts
+++ src/shared/api/words/index.ts
@@ -3,6 +3,7 @@ import { wordsApi } from "../../axios/wordsApi";
 type GetWordsParams = {
 	term: string;
 };
+err;
 
 export const getWords = async (params: GetWordsParams) => {
 	return await wordsApi.get("", { params: params }).then((res) => {
