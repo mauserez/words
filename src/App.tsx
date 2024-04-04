714efd312ba78eed43c2948c88e71467c91f732e
HomeMac <mauserez@mail.ru>
2024-04-04 16:42:14 +0300

    try to add error

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { store, persistor } from "./shared/redux/store";
 import { Layout } from "./widgets/layout/Layout";
 import { SearchWords } from "./widgets/words/search-words/SearchWords";
 import { StarredWords } from "./widgets/words/starred-words/StarredWords";
-
+addd err
 const App = () => {
 	return (
 		<Provider store={store}>
